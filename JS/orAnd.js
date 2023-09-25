66a7b9b15758dc8acf61cd989f3b7dbbbece5204
Akhilender <akhilenderb9@gmail.com>
2023-09-25 23:16:41 +0530

    learning or and and operators in js

---
+++ JS/orAnd.js
@@ -0,0 +1,7 @@
+let isFun = true;
+let isLearning = 0;
+
+console.log(isFun && "Learning is fun!");
+console.log(isFun || "Learning is not fun!");
+
+// try the same above by making isFun = false and isLearning = 1
