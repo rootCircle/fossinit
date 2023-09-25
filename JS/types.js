0385f6a99bbe8126dc2bb31a04464ecf72dffdf8
Akhilender <akhilenderb9@gmail.com>
2023-09-25 23:35:03 +0530

    Learning types present in the javascript

---
+++ JS/types.js
@@ -0,0 +1,10 @@
+console.log(typeof 42);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof [1, 2, 3]);
+console.log(typeof { name: "John", age: 25 });
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof function () {});
+
+// Learn the types 
